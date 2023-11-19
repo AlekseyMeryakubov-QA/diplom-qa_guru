b54c51bcea4c4c2bdd8e50cb5483d503eb63dc30
ameryakubov <alex_meryakubov@mail.ru>
2023-11-19 22:26:42 +0300

    with add new browserSize

--- src/test/java/ru/open/tests/TestBase.java
+++ src/test/java/ru/open/tests/TestBase.java
@@ -26,7 +26,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://open-broker.ru/invest/";
-        Configuration.browserSize = System.getProperty("browser", "1920x1080");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browseVersion", "100.0");
         Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
